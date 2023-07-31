f926126b0ca11b39653482a6460ea111bb51ed5f
Kate Shea <daishea@gmail.com>
2023-07-31 12:03:27 +0530

    Moved map on location page

--- src/components/Location.js
+++ src/components/Location.js
@@ -20,12 +20,12 @@ const Location = () => {
         </div>
       </div>
       <div className="directions-flex">
+      <h2 className="title">GETTING HERE</h2>
       <div className="map-container">      
           <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15868.545550563864!2d80.11253421003889!3d6.112333738216121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae177a7a0804ed5%3A0xe3cc6de683f1f908!2sCoconut%20Club%20Beach%20Cafe!5e0!3m2!1sen!2slk!4v1683359009008!5m2!1sen!2slk" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
             </iframe>
         </div>
         <div className="directions-container">
-          <h2 className="title">GETTING HERE</h2>
           <div className="direction-text main-text">
               <h3>FROM HIKKADUWA TOWN</h3>
               <p>We are approximately 2 km away from the Hikkaduwa main point / Hotel Vibration. Head towards Galle, passing by the Hotel Lavanga. Keep an eye out for our Coconut Club  sign on the right.</p>
